41a24fae90f3f897d22aeb8a05cb7a2044fdf157
veeck <michael.veeck@nebenan.de>
2023-01-16 11:37:09 +0100

    Conversion part 1

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,30 @@
+type Listener = {
+  interval: number,
+  callback: () => void,
+  called: number
+};
+
+type Listeners = { [id: number]: Listener };
+
 const HEARTBEAT_INTERVAL = 1000 * 10; // 10sec
-
 const isAlive = typeof window !== 'undefined';
+const listeners:Listeners = {};
 
-const listeners = {};
 let lastIndex = 0;
-let heartbeatTid: number | null | undefined = null;
+let heartbeatTid: ReturnType<typeof setTimeout> | undefined;
 
 const removeListener = (id: number) => {
-  // @ts-ignore
   if (!listeners[id]) return;
-  // @ts-ignore
   delete listeners[id];
 };
 
-/**
- * @function
- * @param {number} interval
- * @param {function} callback
- * @return {function(): void}
- */
-const addListener = (interval: any, callback: any) => {
+const addListener = (interval: number, callback: (...args: any[]) => ReturnType<typeof removeListener>) => {
   if (typeof callback !== 'function') throw new Error('Listener function required');
   lastIndex += 1;
 
   const id = lastIndex;
   const called = Date.now();
 
-  // @ts-ignore
   listeners[id] = { interval, callback, called };
 
   return () => removeListener(id);
@@ -35,24 +33,22 @@ const addListener = (interval: any, callback: any) => {
 const heartbeatLoop = () => {
   const now = Date.now();
 
-  Object.keys(listeners).forEach((id) => {
-    // @ts-ignore
-    const item = listeners[id];
+  for (const key of Object.keys(listeners).map(Number)) {
+    const item = listeners[key];
     // Item may have been deleted during iteration cycle
     if (item && (now - item.called > item.interval)) {
       item.callback();
       item.called = now;
     }
-  });
+  }
 
   heartbeatTid = setTimeout(heartbeatLoop, HEARTBEAT_INTERVAL);
 };
 
 const handleVisibilityChanged = () => {
   if (document.hidden) {
-    // @ts-ignore
     clearTimeout(heartbeatTid);
-    heartbeatTid = null;
+    heartbeatTid = undefined;
   } else heartbeatLoop();
 };
 
